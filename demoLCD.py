8da2c39401a7a4c2439919619d51c3beb28302f9
Brian Logan <id@brianjlogan.com>
2020-04-02 09:10:49 -0400

    demoLCD

---
+++ demoLCD.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'demoLCD.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(265, 162)
+        self.lcdNumber = QLCDNumber(Dialog)
+        self.lcdNumber.setObjectName(u"lcdNumber")
+        self.lcdNumber.setGeometry(QRect(80, 60, 100, 40))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+    # retranslateUi
+
